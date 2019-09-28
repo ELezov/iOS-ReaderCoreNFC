2eab9f84c95a475a471fab1fd06074b617207a0d
ELezov <eugene.lezov@gmail.com>
2019-09-28 07:27:06 +0200

    add reader function

---
+++ ReaderCoreNFC/Manager/NFCReaderManager.swift
@@ -0,0 +1,13 @@
+//
+//  NFCReaderManager.swift
+//  ReaderCoreNFC
+//
+//  Created by EugenKGD on 28/09/2019.
+//  Copyright © 2019 EugenKGD. All rights reserved.
+//
+
+import CoreNFC
+
+class NFCReaderManager {
+    
+}
--- ReaderCoreNFC/ViewController.swift
+++ ReaderCoreNFC/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
+    var nfcSession: NFCNDEFReaderSession?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func scanPressed(_ sender: Any) {
+        // this is our newly created IBAction
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
     }
 
+}
 
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invalidated: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        // Parse the card's information
+        var result = ""
+        for payload in messages[0].records {
+            result += String.init(data: payload.payload.advanced(by: 3), encoding: .utf8)! // 1
+        }
+        
+        DispatchQueue.main.async {
+            self.messageLabel.text = result
+        }
+    }
 }
 
